016fbd9d5c26beaa2d77051005544f3cfa867a12
immaazkhan <117020351+immaazkhan@users.noreply.github.com>
2022-11-20 04:46:12 -0800

    Update server.js

--- server.js
+++ server.js
@@ -67,7 +67,8 @@ app.use(express.static(path.join(__dirname, "client", "build")))
 // ...
 // Right before your app.listen(), add this:
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  //  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	res.sendFile("hello"));
 });
 
 const port = process.env.PORT || 3001;
